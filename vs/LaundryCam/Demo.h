9edd6e74673aee20e081da56dac4701b62efa5d8
abay102 <86785944+abay102@users.noreply.github.com>
2021-07-11 00:42:30 +0700

    LaundryCam

---
+++ vs/LaundryCam/Demo.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, mesincuciVBO, mesincuciVAO, mesincuciEBO, mesincuciTexture, VBO2, VAO2, EBO2, texture2;
+	GLuint boxVBO, boxVAO, boxEBO, boxTexture;
+	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow* window);
+	void Buildmesincuci();
+	void BuildColoredPlane();
+	void Drawmesincuci();
+	void DrawColoredPlane();
+	void MoveCamera(float speed);
+	void StrafeCamera(float speed);
+	void RotateCamera(float speed);
+	void InitCamera();
+	void Buildbox();
+	void Drawbox();
+};
+
